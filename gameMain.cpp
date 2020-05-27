6fc189f129b8b3134418f7393ed4f0f422d98050
Arnoldo Alvarez <arnoldo.alvarez75@yahoo.com>
2020-05-27 20:36:22 +0000

    Quitando todo

--- gameMain.cpp
+++ gameMain.cpp
@@ -24,8 +24,6 @@ cout<<"nivel de juego papa2: "<<mapa.gameLevel<<endl;
 	switch (mapa.gameLevel)
 	{
 		case 1:
-			cout<<"Probando GITHUB y es super nice"<<endl;
-                        cout<<"esta entrando en case 1"<<endl;
 			mapa.drawMap();
 			cout<<"Utiliza (w)=arriba,(a)Izquierda,(s)Abajo,(d)Derecha,(q)Quit..."<<endl;
 			mapa.isGameOver = false;
