16465f2efc56c25858131634f8231bd33cb113c1
Arnoldo Alvarez <arnoldo.alvarez75@yahoo.com>
2020-05-27 16:40:44 -0400

    Probando de Ubuntu to Mac

--- gameMain.cpp
+++ gameMain.cpp
@@ -24,7 +24,7 @@ cout<<"nivel de juego papa2: "<<mapa.gameLevel<<endl;
 	switch (mapa.gameLevel)
 	{
 		case 1:
-			cout<<"Probando despues de haber quitado las keys en gitHub"
+			cout<<"Ahora probando desde Ubuntu to Mac"
 			mapa.drawMap();
 			cout<<"Utiliza (w)=arriba,(a)Izquierda,(s)Abajo,(d)Derecha,(q)Quit..."<<endl;
 			mapa.isGameOver = false;
